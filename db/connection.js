902a013558deeb5c7acfb44de12f89f949005116
Riyakhandelwl <riyakhandelwal1401@gmail.com>
2024-07-04 10:12:50 +0530

    done

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const connectDB=()=>{
+    mongoose.connect("mongodb+srv://riyakhandelwal1401:riya1234@bloging.p8o1hbk.mongodb.net/")
+    
+.then(()=>{
+    console.log("successfully connected to mongodb")
+}).catch((e)=>{
+    console.log(e)
+})
+}
+
+export default connectDB;
