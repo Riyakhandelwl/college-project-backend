14e4371bd43294c7852192790bc922ac9db524e0
Riyakhandelwl <139372039+Riyakhandelwl@users.noreply.github.com>
2024-07-08 12:04:18 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectDB=()=>{
-    mongoose.connect("mongodb+srv://riyakhandelwal1401:riya1234@bloging.p8o1hbk.mongodb.net/")
+    mongoose.connect("mongodb+srv://riyakhandelwal1401:riya1234@bloging.p8o1hbk.mongodb.net/bloging")
     
 .then(()=>{
     console.log("successfully connected to mongodb")
